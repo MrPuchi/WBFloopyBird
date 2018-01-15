a37a51be50c52d4897fd37b2a024777e13f61ec0
MrPuchi <juanzacarias0@gmail.com>
2018-01-15 11:23:17 -0500

    Creating Bird controls

---
+++ WBFLoppyBird/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private float x;
+    private float y;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        y = -1*Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.Translate(x, y, 0);
+
+    }
+}
